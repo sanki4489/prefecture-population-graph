3cef60acde51a6aede424827409989230070abe4
VERMA SANKALP <sankarupu.uruma@nsw.co.jp>
2024-07-16 10:01:26 +0900

    chore: Setup development environment

---
+++ src/App.spec.tsx
@@ -0,0 +1,11 @@
+import { describe, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App.tsx";
+
+describe("App を render したとき", () => {
+  test("It should render hello world", () => {
+    render(<App />);
+
+    expect(screen.getByText("hello world")).toBeInTheDocument();
+  });
+});

4ebb38e7e9c07a39c281fbf2839780c0a1b9cefb
VERMA SANKALP <sankarupu.uruma@nsw.co.jp>
2024-07-16 11:08:43 +0900

    chore: add color utilities

---
+++ src/utils/ColorList.ts
@@ -0,0 +1,50 @@
+export const ColorList = [
+  '#00FA9A', // Medium Spring Green
+  '#6495ED', // Cornflower Blue
+  '#FF4500', // Orange Red
+  '#32CD32', // Lime Green
+  '#BA55D3', // Medium Orchid
+  '#B8860B', // Dark Goldenrod
+  '#FFA500', // Orange
+  '#7B68EE', // Medium Slate Blue
+  '#3CB371', // Medium Sea Green
+  '#DC143C', // Crimson
+  '#FFD700', // Gold
+  '#808000', // Olive
+  '#FF8C00', // Dark Orange
+  '#4682B4', // Steel Blue
+  '#008000', // Dark Green
+  '#FF69B4', // Hot Pink
+  '#008080', // Teal
+  '#9932CC', // Dark Orchid
+  '#FFA07A', // Light Salmon
+  '#800080', // Purple
+  '#2E8B57', // Sea Green
+  '#FF0000', // Red
+  '#00FF00', // Green
+  '#D2691E', // Chocolate
+  '#0000FF', // Blue
+  '#FF1493', // Deep Pink
+  '#FF00FF', // Magenta
+  '#000080', // Navy
+  '#00FFFF', // Cyan
+  '#FFFF00', // Yellow
+  '#7FFF00', // Chartreuse
+  '#5F9EA0', // Cadet Blue
+  '#ADFF2F', // Green Yellow
+  '#BA55D3', // Medium Orchid
+  '#800000', // Maroon
+  '#E9967A', // Dark Salmon
+  '#8B008B', // Dark Magenta
+  '#CD5C5C', // Indian Red
+  '#FF4500', // Orange Red
+  '#FF0000', // Red
+  '#FF69B4', // Hot Pink
+  '#008000', // Dark Green
+  '#FFFF00', // Yellow
+  '#FF8C00', // Dark Orange
+  '#000080', // Navy
+  '#FFA500', // Orange
+  '#D2691E', // Chocolate
+  '#4B0082' // Indigo
+];
